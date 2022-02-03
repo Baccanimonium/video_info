9573462f63cc1480fa68e47602bf849a1e79f7f7
Bacca <forinventorwork@mail.ru>
2022-02-03 13:41:23 +0300

    правка верстки

--- src/Pages/Tab/Pages/SelectionCriteria/Components/styles.js
+++ src/Pages/Tab/Pages/SelectionCriteria/Components/styles.js
@@ -9,3 +9,14 @@ export const StyleTrashIcon = styled(TrashIcon)`
     color: var(--color-gold-2);
   }
 `
+export const IconPlus = styled.div`
+  .svg-inline--fa {
+    width: 16px;
+    height: 16px;
+    color: var(--color-grey-darken-0);
+    &:hover  {
+      color: var(--color-gold-2);
+    }
+  }
+  
+`
