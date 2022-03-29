725ed8f300c9601f38a96125c8a2a8626caeb693
Bacca <forinventorwork@mail.ru>
2022-03-29 13:42:36 +0300

    страница логина переделана на функциональный компонент правка кода на ней

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react"
+import React, {Component, useState} from "react"
 import PropTypes from "prop-types"
 import StateLessForm from "@/Components/Forms/StateLessForm"
 import BsButton from "@/Components/BsButton"
 import { AuthRequest } from "@/Store/userObject"
 import WithOpenModalWindow from "@/Core/Decorators/WithOpenModalWindow"
-import history from "@/history"
+import { useNavigate } from "react-router-dom";
 import {
   PageContainer, ContentContainer, FormContainer, FormTittle, ImgLogo
 } from "./styles"
@@ -13,59 +13,51 @@ import { rules, fieldMap } from "./formConfig"
 
 const LOCAL_STORAGE_REMEBER_ME = "LOCAL_STORAGE_REMEBER_ME"
 
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: {
-        storeCredentials: Boolean(localStorage.getItem(LOCAL_STORAGE_REMEBER_ME))
-      },
-      loading: false,
+const Login = ({onSubmit, initialRoute, openModalWindow}) => {
+  const [value, setValue] = useState({
+    storeCredentials: Boolean(localStorage.getItem(LOCAL_STORAGE_REMEBER_ME))
+  })
+  const [loading, setLoading] = useState(false)
+
+  let navigate = useNavigate();
+
+  const handleInput = (payload) => {
+    setValue({ ...value, ...payload } )
+  }
+
+  const submitForm = async ({ storeCredentials, login, password }) => {
+    try {
+      setLoading(true)
+      if (login === "user" && password === "11111") {
+        localStorage.setItem(LOCAL_STORAGE_REMEBER_ME, String(Boolean(storeCredentials)))
+        // const token = await AuthRequest({ login, password })
+        const token = "token"
+
+        if (storeCredentials && window.PasswordCredential) {
+          await navigator.credentials.store(await new window.PasswordCredential({ id: login, password }))
+        }
+        onSubmit(token)
+        if (initialRoute) {
+          navigate(initialRoute)
+        } else {
+          navigate("/tab")
+        }
+      } else {
+        openModalWindow({
+          message: "Couldn't connect to login server. Please contact your administrator"
+        })
+      }
+    } catch (e) {
+      openModalWindow({
+        message: "Couldn't connect to login server. Please contact your administrator"
+      })
+      console.log("login error", e)
+    } finally {
+      setLoading(false)
     }
   }
 
-  handleInput = (payload) => {
-    this.setState(({ value }) => ({ value: { ...value, ...payload } }))
-  }
 
-  submitForm = async ({ storeCredentials, login, password }) => {
-    const { onSubmit, initialRoute, openModalWindow } = this.props
-    if (login === "user" && password === "11111") {
-      history.push("/tab")
-    } else openModalWindow({
-          message: "Couldn't connect to login server. Please contact your administrator"
-        })
-    // try {
-    //   this.setState({ loading: true })
-    //   localStorage.setItem(LOCAL_STORAGE_REMEBER_ME, String(Boolean(storeCredentials)))
-    //   // const token = await AuthRequest({ login, password })
-    //   const token = "token"
-    //
-    //   if (storeCredentials && window.PasswordCredential) {
-    //     await navigator.credentials.store(await new window.PasswordCredential({ id: login, password }))
-    //   }
-    //   onSubmit(token)
-    //   if (initialRoute) {
-    //     history.push(initialRoute)
-    //   } else {
-    //     history.push("/tab")
-    //   }
-    // } catch (e) {
-    //   openModalWindow({
-    //     message: "Couldn't connect to login server. Please contact your administrator"
-    //   })
-    //   console.log("login error", e)
-    // } finally {
-    //   this.setState({ loading: false })
-    // }
-  }
-
-  onInput = (payload) => {
-    this.setState({ val: payload })
-  }
-
-  render() {
-    const { state: { value, loading } } = this
     return (
       <PageContainer>
         <ImgLogo src="./assets/bg/img-login.jpg" alt="" />
@@ -78,8 +70,8 @@ class Login extends Component {
               fields={fieldMap}
               rules={rules}
               value={value}
-              onInput={this.handleInput}
-              onSubmit={this.submitForm}
+              onInput={handleInput}
+              onSubmit={submitForm}
             >
               <BsButton
                 className="golden btn width-max m-l-a m-r-a m-t-20"
@@ -95,7 +87,7 @@ class Login extends Component {
         </ContentContainer>
       </PageContainer>
     )
-  }
+
 }
 
 Login.propTypes = {
