0fb07d7b688098fdb4dd0ea0298861e28d0db7ba
Bacca <forinventorwork@mail.ru>
2022-03-31 11:30:47 +0300

    временно вернула код перехода на станицу Наборы данных

--- src/Pages/Tab/index.js
+++ src/Pages/Tab/index.js
@@ -4,10 +4,18 @@ import NavigationDrawer from "@/Components/NavigationDrawer"
 import TabBar from "@/Components/TabBar"
 import {tabNavigationMenu} from "./constants"
 import PureDeleteItems from "@/Utils/Arrays/PureDeleteItems"
+import Icon from "@/Components/Icon"
+import {plusIcon} from "./icons/plusIcon"
+import WithOpenContextMenu from "@/Core/RenderProps/WithOpenContextMenu"
 import Home from "./Pages/Home";
 import NewTask from "../NewTask";
 import DownloadTask from "../DownloadTask";
 import {RouteContext} from "../../constants"
+import DataSet from "@/Pages/Tab/Pages/DataSet";
+import Reports from "@/Pages/Tab/Pages/Reports";
+import Result from "@/Pages/Tab/Pages/Result";
+import SelectionCriteria from "@/Pages/Tab/Pages/SelectionCriteria"
+const PlusIcon = Icon(plusIcon)
 
 const Tab = (a) => {
   const path = "/tab"
@@ -31,6 +39,29 @@ const Tab = (a) => {
     setTabIndex(newIndex)
   }, [])
 
+  const OpenEditForm = useCallback(async ({applyContextMenu}) => {
+    await applyContextMenu([
+      {
+        componentProps: {
+          title: "Наборы данных",
+        },
+        onSubmit: onOpenNewTab,
+      },
+      {
+        componentProps: {
+          title: "Новый набор",
+        },
+        onSubmit: onOpenNewTab,
+      },
+      {
+        componentProps: {
+          title: "Открыть папку",
+        },
+        onSubmit: onOpenNewTab
+      },
+    ])
+  }, [])
+
   return (
     <RouteContext.Provider value={useMemo(() => ({ path, onOpenNewTab }), [onOpenNewTab, path])}>
       <div className="display-flex h-100">
@@ -46,26 +77,64 @@ const Tab = (a) => {
             onCloseTab={closeTab}
             onChangeActiveTab={onChangeActiveTab}
           >
-          </TabBar>
-          {tabs.length > 0
-            ?
-             <>
-               <Routes>
-                 <Route
-                   path="/new_task"
-                   element={<NewTask/>}
-                 />
-                 <Route
-                   path="/download_task"
-                   element={<DownloadTask/>}
-                 />
-               </Routes>
-             </>
-            :
-            <Home/>
-          }
-        </div>
+            <WithOpenContextMenu
+              settings={{maxSize: "200", minSize: "200"}}
+              onOpenContextMenu={OpenEditForm}
+            >
+            {(onOpenContextMenu) => (
+              <button
+                type="button"
+                className="min-gold-button btn bg-color-lightGold"
+                onClick={onOpenContextMenu}
+              >
+                <PlusIcon
+                  className="color-white"
+                  size="22"
+                />
+              </button>
+            )}
+          </WithOpenContextMenu>
+
+        </TabBar>
+        {tabs.length > 0
+          ?
+           <>
+             {/*<TabHeader*/}
+             {/*  path={urls[location.pathname]}*/}
+             {/*/>*/}
+             <Routes>
+               <Route
+                 path="/new_task"
+                 element={<NewTask/>}
+               />
+               <Route
+                 path="/download_task"
+                 element={<DownloadTask/>}
+               />
+               <Route
+                 path="/data_set"
+                 element={<DataSet/>}
+               />
+               <Route
+                 path="/report"
+                 element={<Reports/>}
+               />
+               <Route
+                 path="/selection_criteria"
+                 element={<SelectionCriteria/>}
+               />
+               <Route
+                 path="/result"
+                 element={<Result/>}
+               />
+             </Routes>
+           </>
+          :
+          <Home/>
+        }
+
       </div>
+    </div>
     </RouteContext.Provider>
   )
 }
